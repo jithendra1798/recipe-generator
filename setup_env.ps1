20ccbb5792533873d642507f7669dfe65b7fae0d
jithendra1798 <siddartha1798@gmail.com>
2025-09-28 12:13:18 -0400

    add setup llm command

--- setup_env.ps1
+++ setup_env.ps1
@@ -10,7 +10,10 @@ python -m pip install --upgrade pip
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
+# Enable LLM transfer
 setx HF_HUB_ENABLE_HF_TRANSFER 1
 
+# Setup llm
+python setup_ll.py
 
 Write-Host "✅ Virtual environment setup complete. Use '.\venv\Scripts\Activate.ps1' to activate it later."
